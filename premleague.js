5f7f97ddd5f9c8f8bfd6ef4cad657bbae10386fa
bengrams16 <32134536+bengrams16@users.noreply.github.com>
2017-09-25 11:26:39 -0400

    Create premleague.js

---
+++ premleague.js
@@ -0,0 +1,8 @@
+
+function moreStuff(){
+document.getElementById("solution").innerHTML =
+"<p>GGMU </p><h4>We're winning the league</h4>" +
+"<p>" +
+" Click this <a href='https://www.premierleague.com/'> link</a></p>"
+
+}
