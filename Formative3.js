f12341e2f90e9a1c1a4a7ed9818f99e383936063
bengrams16 <32134536+bengrams16@users.noreply.github.com>
2017-10-02 11:12:45 -0400

    Create Formative3.js

---
+++ Formative3.js
@@ -0,0 +1,5 @@
+var team =window.prompt(“What house are you in?”);
+window.alert(“I am in” + team + “ house.”);
+if{ team==“Chittenden” 
+mission: background=yellow;}
+else{ window.alert(“Sorry, Chittenden students only.”);}
